6e7d02633830b7d882e2ac1068b377cf35e0ad32
Berekto <bereketalebachew87@gmail.com>
2023-05-06 21:10:47 +0300

    In and Out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- A SQL script that creates a table users
 -- Creates a table user 
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO-INCREMENT PRIMARY KEY, 
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255), 
 	country ENUM('US', 'CO', 'TN') NOT NULL

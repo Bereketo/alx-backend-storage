a4dc654677459fe539ad3088af8da92daacc2fba
Berekto <bereketalebachew87@gmail.com>
2023-05-06 18:14:58 +0300

    glam rock

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- A SQL script that lists all bands with Glam rock as their main style
+-- select all bands with Glam rock as their main style, ranked by thier longevity
+SELECT band_name, (split - formed) AS lifespan 
+From metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;

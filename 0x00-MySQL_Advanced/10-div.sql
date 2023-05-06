dd3318cc2e6e8c7c1fee0f912320ac8cba3114da
Berekto <bereketalebachew87@gmail.com>
2023-05-06 22:50:57 +0300

    safe division

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,6 +2,7 @@
 -- divides (and returns) the first by the second number 
 -- or returns 0 if the second number is equal to 0
 
+DELIMITER $$
 DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURN INT
@@ -14,3 +15,4 @@ BEGIN
 	END IF;
 	RETURN ans;
 END;
+$$

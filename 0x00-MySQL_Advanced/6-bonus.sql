955fc7161a47818040ec2e76ec866caee82f7652
Berekto <bereketalebachew87@gmail.com>
2023-05-06 22:24:11 +0300

    Add bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,11 +1,12 @@
 -- procedure AddBonus
 -- that adds a new correction for a student
+
 CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
 	DECLARE project_id INT;
 
 	-- check if project id exists
-        SELECT id INTO project_id WHERE name = project_name;
+        SELECT id INTO project_id FROM projects WHERE name = project_name;
 
 	IF project_id IS NULL THEN
 		INSERT INTO projects(name) VALUES(project_name);
